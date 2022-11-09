916195f8c4abbe9e557f6eb0d2a75a9ec444344a
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-11-08 22:28:10 -0500

    added the drinks route and basic css

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 
+const PORT = process.env.PORT
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT
+const drinks = require("./models/drinks")
 
 app.get('/', (req, res) => {
     res.send(`Welcome to the Gitpub App!`)
 })
 
+app.get('/drinks', (req, res) => {
+    res.render("drinks_index.ejs")
+})
 
 
 app.listen(PORT, () => {
